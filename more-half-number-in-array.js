743e2f2058adfe239e9e51f6f6562d37c1b22a30
yang.song <yang.song@17zuoye.com>
2022-02-17 18:31:54 +0800

    39.数组中出现次数超过一半的数字

---
+++ more-half-number-in-array.js
@@ -0,0 +1,30 @@
+// 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+// 示例 1:
+// 输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+// 输出: 2
+
+function moreHalfNumberInArray(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+    if (result.length === 0) {
+      result.unshift(current);
+    } else {
+      if (result[0] !== current) {
+        result.shift();
+      } else {
+        result.unshift(current);
+      }
+    }
+  }
+  return result;
+}
+
+const inputValue1 = [1, 2, 3, 2, 2, 2, 5, 4, 2];
+console.time("a");
+const result1 = moreHalfNumberInArray(inputValue1);
+console.timeEnd("a");
+console.log(result1);
