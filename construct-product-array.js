9fe830b947537b8522c5447228b881f588e1d2ac
yang.song <yang.song@17zuoye.com>
2022-02-28 15:05:26 +0800

    66. 构建乘积数组

---
+++ construct-product-array.js
@@ -0,0 +1,36 @@
+// 给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1]，其中 B[i] 的值是
+// 数组 A 中除了下标 i 以外的元素的积, 即 B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。
+
+// 不能使用除法。
+
+// 示例:
+
+// 输入: [1,2,3,4,5]
+// 输出: [120,60,40,30,24]
+
+function constructProductArray(arr) {
+  const len = arr.length;
+  const leftArr = new Array(len);
+  const rightArr = new Array(len);
+  leftArr[0] = 1;
+  rightArr[len - 1] = 1;
+  for (let i = 1; i < arr.length; i++) {
+    leftArr[i] = arr[i - 1] * leftArr[i - 1];
+  }
+  for (let i = arr.length - 2; i >= 0; i--) {
+    console.log(rightArr);
+    rightArr[i] = arr[i + 1] * rightArr[i + 1];
+  }
+  const result = [];
+  for (let i = 0; i < len; i++) {
+    result[i] = leftArr[i] * rightArr[i];
+  }
+  console.log(leftArr, rightArr);
+  return result;
+}
+
+const inputValue1 = [1, 2, 3, 4, 5];
+console.time("a");
+const result1 = constructProductArray(inputValue1);
+console.timeEnd("a");
+console.log(result1);
