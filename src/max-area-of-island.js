0f25fa212f132ff0a6edcdd042dc64f2fd9e7660
yang.song <yang.song@17zuoye.com>
2023-05-30 19:05:34 +0800

    最大岛屿面积

--- src/max-area-of-island.js
+++ src/max-area-of-island.js
@@ -5,6 +5,44 @@
 
 const { test } = require('./tools/test');
 
-function maxAreaOfIsland(arr) {}
+function isInArea(area, r, c) {
+  return r >= 0 && c >= 0 && r < area.length && c < area[0].length;
+}
 
-test(maxAreaOfIsland);
+function dfs(area, r, c) {
+  if (!isInArea(area, r, c)) {
+    return 0;
+  }
+  if (area[r][c] != 1) {
+    return 0;
+  }
+  area[r][c] = 2;
+
+  return (
+    1 +
+    dfs(area, r - 1, c) +
+    dfs(area, r + 1, c) +
+    dfs(area, r, c - 1) +
+    dfs(area, r, c + 1)
+  );
+}
+
+function maxAreaOfIsland(area) {
+  let res = 0;
+  for (let i = 0; i < area.length; i++) {
+    for (let j = 0; j < area[0].length; j++) {
+      if (area[i][j] == 1) {
+        res = Math.max(res, dfs(area, i, j));
+      }
+    }
+  }
+  return res;
+}
+
+const inputValue = [
+  [0, 1, 0, 1, 1],
+  [1, 1, 1, 0, 0],
+  [1, 1, 0, 0, 1],
+  [0, 1, 0, 1, 1],
+];
+test(maxAreaOfIsland, inputValue);
